212e60d1106fb593fa266b123ebf0e74f377ba10
sanele23 <sanele23@live.com>
2025-07-02 13:47:03 +0200

    update json, including api url

--- src/components/App.js
+++ src/components/App.js
@@ -9,7 +9,7 @@ import NextButton from "./NextButton";
 import Progress from "./Progress";
 import FinishScreen from "./FinishScreen";
 import Footer from "./Footer";
-import Timer from './Timer';
+import Timer from "./Timer";
 
 const SECS_PER_QUESTION = 30;
 
@@ -17,90 +17,99 @@ const initialState = {
   questions: [],
 
   // loading, error, ready, active, finished
-  status: 'loading',
+  status: "loading",
   index: 0,
   answer: null,
   points: 0,
   highscore: 0,
   secondsRemaining: null,
-}
+};
 
 function reducer(state, action) {
   switch (action.type) {
-    case 'dataReceived':
+    case "dataReceived":
       return {
         ...state,
         questions: action.payload,
         status: "ready",
-    };
-
-    case 'dataFailed':
-      return {
-        ...state,
-        status: 'error',
       };
 
-    case 'start':
+    case "dataFailed":
       return {
         ...state,
-        status: 'active',
+        status: "error",
+      };
+
+    case "start":
+      return {
+        ...state,
+        status: "active",
         secondsRemaining: state.questions.length * SECS_PER_QUESTION,
       };
-    
-    case 'newAnswer':
+
+    case "newAnswer":
       const question = state.questions.at(state.index);
       return {
         ...state,
         answer: action.payload,
-        points: action.payload === question.correctOption 
-          ? state.points + question.points 
-          : state.points,
+        points:
+          action.payload === question.correctOption
+            ? state.points + question.points
+            : state.points,
       };
 
-    case 'nextQuestion':
+    case "nextQuestion":
       return {
         ...state,
         index: state.index + 1,
         answer: null,
       };
-    
-    case 'finish':
+
+    case "finish":
       return {
         ...state,
-        status: 'finished',
-        highscore: state.points > state.highscore ? state.points : state.highscore,
-      }
-    
-    case 'restart':
-      return {
-        ...initialState, questions: state.questions,
-        status: 'ready',
+        status: "finished",
+        highscore:
+          state.points > state.highscore ? state.points : state.highscore,
       };
 
-    case 'tick':
+    case "restart":
+      return {
+        ...initialState,
+        questions: state.questions,
+        status: "ready",
+      };
+
+    case "tick":
       return {
         ...state,
         secondsRemaining: state.secondsRemaining - 1,
-        status: state.secondsRemaining === 0 ? 'finished' : state.status,
-      }
-      
+        status: state.secondsRemaining === 0 ? "finished" : state.status,
+      };
+
     default:
       throw new Error("Action unknown");
   }
 }
 
 export default function App() {
-  const [{questions, status, index, answer, points, highscore, secondsRemaining }, dispatch] = useReducer(reducer, initialState)
+  const [
+    { questions, status, index, answer, points, highscore, secondsRemaining },
+    dispatch,
+  ] = useReducer(reducer, initialState);
 
   const numQuestions = questions.length;
-  const maxPossiblePoints = questions.reduce((prev, cur) => prev + cur.points, 0)
+  const maxPossiblePoints = questions.reduce(
+    (prev, cur) => prev + cur.points,
+    0
+  );
 
   // retreive questions -- http://localhost:8000/questions
   useEffect(function () {
-    fetch("http://localhost:9000/questions")
+    fetch("https://react-quiz-3cey.onrender.com")
       .then((res) => res.json())
-      .then((data) => dispatch({ type: 'dataReceived', payload: data }))
-      .catch((err) => dispatch({ type: 'dataFailed' }));
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
   }, []);
 
   return (
@@ -108,47 +117,46 @@ export default function App() {
       <Header />
 
       <Main>
-        {status === 'loading' && <Loader />}
-        {status === 'error' && <Error />}
-        {status === 'ready' && <StartScreen numQuestions={numQuestions} dispatch={dispatch} />}
-        {status === 'active' && (
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && (
+          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
+        )}
+        {status === "active" && (
           <>
-          <Progress 
-            index={index} 
-            numQuestions={numQuestions} 
-            points={points} 
-            maxPossiblePoints={maxPossiblePoints}
-            answer={answer}
-          />
+            <Progress
+              index={index}
+              numQuestions={numQuestions}
+              points={points}
+              maxPossiblePoints={maxPossiblePoints}
+              answer={answer}
+            />
 
-            <Question 
-              question={questions[index]} 
-              dispatch={dispatch} 
-              answer={answer} 
+            <Question
+              question={questions[index]}
+              dispatch={dispatch}
+              answer={answer}
             />
             <Footer />
-            <Timer 
+            <Timer dispatch={dispatch} secondsRemaining={secondsRemaining} />
+
+            <NextButton
               dispatch={dispatch}
-              secondsRemaining={secondsRemaining} 
-            />
-            
-            <NextButton 
-              dispatch={dispatch} 
-              answer={answer} 
-              numQuestions={numQuestions} 
-              index={index} 
+              answer={answer}
+              numQuestions={numQuestions}
+              index={index}
             />
           </>
-          
         )}
-        {status === 'finished' && 
-          <FinishScreen 
-            points={points} 
-            maxPossiblePoints={maxPossiblePoints} 
+        {status === "finished" && (
+          <FinishScreen
+            points={points}
+            maxPossiblePoints={maxPossiblePoints}
             highscore={highscore}
             dispatch={dispatch}
-          /> }
+          />
+        )}
       </Main>
     </div>
-  )
+  );
 }
